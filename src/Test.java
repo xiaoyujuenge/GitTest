90c17079f9c5e8590a5cba52d317a5f6fc5b18c4
ljh <xiaoyujuenge@163.com>
2019-01-02 21:00:41 +0800

    three

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,12 @@
 public class Test {
+    /**
+     * 程序入口
+     * @param args
+     */
     public static void main(String[] args) {
+        //打印语句
         System.out.println("ok");
+        System.out.println("no");
                 
     }
 }
